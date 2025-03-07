e802d5f6fca306a71586f6e20d19d687809347ad
Hüseyin Sancak <huseyin.sancak@gmail.com>
2025-03-07 09:30:05 +0300

    Yeni Proje Eklendi

---
+++ NetCoreAI.Project2.ApiConsumeUI/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAI.Project2.ApiConsumeUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult CustomerList()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCoreAI.Project2.ApiConsumeUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreAI.Project1.ApiConsumeUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
