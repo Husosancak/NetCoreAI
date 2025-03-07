4bcd2dd37935a2944a8257915eae552db49db29e
Hüseyin Sancak <huseyin.sancak@gmail.com>
2025-03-07 09:15:08 +0300

    Proje 1

---
+++ NetCoreAI.Project1.ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1.ApiDemo.Entities;
+
+namespace NetCoreAI.Project1.ApiDemo.Context
+{
+    public class ApiContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=192.168.10.40;Database =ApiDemo;Persist Security Info=True;User ID=sa;Password=1q2w3easdP@ssw0rd;TrustServerCertificate=True");
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
